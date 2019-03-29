74ae468059c15a700df42f5ee0b15f3b5083626f
John Bohannon <imjohnbo@github.com>
2019-03-29 11:25:11 -0700

    fix: type check

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// @todo add type check
+// add numbers
 function addNums(a,b) {
-    return a + b;
+    if (typeof a === 'number' &&
+        typeof b === 'number') {
+        return a + b;
+    }
 }
